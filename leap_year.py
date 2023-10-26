56318ad7ada5e1f7f410c03d633b8a43c099e4cc
BLDZ <69957760+BLDZ@users.noreply.github.com>
2023-10-26 14:24:43 +0200

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -6,5 +6,3 @@ class LeapYear:
         elif (year % 4 != 0) or (year % 100 == 0 and year % 400 != 0):
             print(f'The year {year} is not a leap year.')
             return False
-
-#Ingen endring
